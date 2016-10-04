c1fc016dbce11fb544ca42ff3a43126faebc6019
adrewniekamp <andrew.f.niekamp@gmail.com>
2016-10-04 11:12:26 -0700

    add dbcontext, and add it to startup

---
+++ src/MVCWebApp/Models/MVCWebAppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCWebApp.Models
+{
+    public class MVCWebAppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=MVCWebApp;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
